33c1f6aef9670a5f0f03a5363c1f4fc6ba1f15dc
Faria <fariaguga@hotmail.com>
2022-05-19 19:07:35 -0300

    feat: add Tasks model

---
+++ models/task.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+module.exports = (sequelize) => {
+  const Task = sequelize.define(
+    'Task',
+    Attributes,
+    {
+      underscore: true,
+      timestamps: false,
+      tableName: 'Tasks',
+    },
+  );
+  
+  return Task;
+};
