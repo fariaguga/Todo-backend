483c24778af39b287117869d5d8e92abdc03d985
Faria <fariaguga@hotmail.com>
2022-05-19 19:07:12 -0300

    feat: add Tasks migration

---
+++ migrations/20220519212739-create-task.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
