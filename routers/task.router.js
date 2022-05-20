dc7318b2b272a39276da4ee69f77e9c40b21ab89
Faria <fariaguga@hotmail.com>
2022-05-20 18:43:02 -0300

    feat: add /task/:id DELETE and PUT  route

--- routers/task.router.js
+++ routers/task.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { addTask } = require('../controllers/task.controller');
 const { getAllTasks } = require('../controllers/task.controller');
+const { getById } = require('../controllers/task.controller');
+const { editTask } = require('../controllers/task.controller');
+const { deleteTask } = require('../controllers/task.controller');
 const middlewares = require('../middlewares/index');
 
 const router = express.Router();
@@ -11,6 +14,10 @@ const validateTask = [
   ];
 
 router.get('/', getAllTasks);
+router.get('/:id', getById);
 router.post('/', validateTask, addTask);
+router.put('/:id', editTask);
+router.delete('/:id', deleteTask);
+
 
 module.exports = router;
